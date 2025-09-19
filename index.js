f429d2ec69eb4ca20e652fdaa76b4ca86a7907cc
AdemolaAl <ademolaalameen86@gmail.com>
2025-09-19 15:34:05 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/pictures', express.static(process.cwd() + '/pictures'));
+
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname + '/views/index.html');
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
